26e5a48772f5468b2bafde5c860e9853ac0c5328
Bruna Gomes <heathjared2@gmail.com>
2022-11-19 17:23:28 -0300

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    token: ''
+  },
+  getters: {
+  },
+  mutations: {
+    setToken(state, token) {
+      state.token = token;
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
